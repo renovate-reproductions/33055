7756501b3bab102ec97812e3f5b23029582eb923
David Martos <davidmartos96@gmail.com>
2024-12-12 00:27:26 +0100

    add dart project

---
+++ bin/dart_renovatebot_bug.dart
@@ -0,0 +1,5 @@
+import 'package:dart_renovatebot_bug/dart_renovatebot_bug.dart' as dart_renovatebot_bug;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_renovatebot_bug.calculate()}!');
+}
---
+++ test/dart_renovatebot_bug_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_renovatebot_bug/dart_renovatebot_bug.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
